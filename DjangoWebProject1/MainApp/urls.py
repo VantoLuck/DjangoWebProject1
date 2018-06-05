0c4eed28383464b1fff0193fab13457833b21d0f
VantoLuck <leonidkartushin@gmail.com>
2018-06-06 00:44:35 +0300

    Try1

---
+++ DjangoWebProject1/MainApp/urls.py
@@ -0,0 +1,13 @@
+"""
+Definition of urls for DjangoWebProject1.
+"""
+
+from django.conf.urls import include, url
+from . import views
+
+
+urlpatterns = [
+   
+    url(r'^$', views.index, name='index'),
+     url(r'^contact/$', views.contact, name='contact'),
+]
---
+++ DjangoWebProject1/MainApp/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request,'MainApp/homepage.html')
+
+def contact(request):
+    return render(request,'MainApp/basic.html', {'values': ['Если у вас остались вопросы, то можете их не задавать по этому номеру','8-(916)-901-20-30','example@mail.ru']})
